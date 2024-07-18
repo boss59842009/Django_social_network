c5c3c88dfc97f131573c03c7a81bbea711f8fde4
Jenya <boss59842009@gmail.com>
2024-07-18 17:06:41 +0300

    add PostUpdateView, post_edit_form.html

--- posts/mixins.py
+++ posts/mixins.py
@@ -4,6 +4,7 @@ from django.core.exceptions import PermissionDenied
 class UserIsAuthorMixin(object):
     def dispatch(self, request, *args, **kwargs):
         instance = self.get_object()
-        if instance.user == request.user:
+        instance = self.get_object()
+        if instance.user != request.user:
             raise PermissionDenied
         return super().dispatch(request, *args, **kwargs)
--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('posts/all/', views.AllPostsView.as_view(), name='all-posts'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/create/', views.PostCreateView.as_view(), name='post-create'),
-    path('like/<int:pk>/', views.like_counts_view, name='like-post')
+    path('post/<int:pk>/like/', views.like_unlike_view, name='like-post'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='edit-post')
 ]
