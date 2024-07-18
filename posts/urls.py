816c76796933274572a6a161c73fc4d22ea0feb2
Jenya <boss59842009@gmail.com>
2024-07-18 15:35:34 +0300

    add like_counts_view, edit PostDetailView and post_detail.

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from posts import views
 urlpatterns = [
     path('posts/all/', views.AllPostsView.as_view(), name='all-posts'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('post/create/', views.PostCreateView.as_view(), name='post-create')
+    path('post/create/', views.PostCreateView.as_view(), name='post-create'),
+    path('like/<int:pk>/', views.like_counts_view, name='like-post')
 ]
