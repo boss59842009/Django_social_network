4474956e56f1450d7b8542c619d8185c37db3f8b
Jenya <boss59842009@gmail.com>
2024-11-17 15:54:05 +0200

    remove all ignored files

--- posts/mixins.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import PermissionDenied
-
-
-class UserIsAuthorMixin(object):
-    def dispatch(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.author != request.user:
-            raise PermissionDenied
-        return super().dispatch(request, *args, **kwargs)
--- static/js/create_post_modal.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('create_post').addEventListener('click', function () {
-  var modal = new bootstrap.Modal(document.getElementById('new_post'));
-  modal.show();
-});
