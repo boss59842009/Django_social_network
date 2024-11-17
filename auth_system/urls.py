92fc0f44d74e71d741bd1b3c98f8076f624d5816
Jenya <boss59842009@gmail.com>
2024-11-17 14:35:11 +0200

    add login via google with django-allauth

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -1,14 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from auth_system import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.user_login_view, name='login'),
+    # path('login/', views.user_login_view, name='login'),
+    path('accounts/login/', views.user_login_view, name='login'),
+    path('accounts/', include('allauth.urls')),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-    path('registration_user/', views.UserRegistrationView.as_view(), name='registration'),
-    path('edit_user/<slug:slug>', views.edit_profile_view, name='user-edit'),
+    path('registration-user/', views.UserRegistrationView.as_view(), name='registration'),
+    path('edit-user/<slug:slug>', views.edit_profile_view, name='user-edit'),
     path('user/<slug:slug>', views.info_profile_view, name='user-info'),
-    path('all_users/', views.profiles_list_view, name='users-list'),
-    path('followed_users/', views.followed_profiles_list_view, name='followed-users-list'),
+    path('all-users/', views.profiles_list_view, name='users-list'),
+    path('followed-users/', views.followed_profiles_list_view, name='followed-users-list'),
     path('user/<slug:slug>/follow', views.follow_unfollow_profile_view, name='follow-user')
 ]
